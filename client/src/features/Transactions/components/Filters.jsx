ff22e89bda8231a6e6f0b0fd68f09367c7b4626b
Jakub <jm54706@outlook.com>
2025-11-10 21:12:04 +0100

    changed filters

--- client/src/features/Transactions/components/Filters.jsx
+++ client/src/features/Transactions/components/Filters.jsx
@@ -5,20 +5,24 @@ import Header2 from '../../../components/Header2/Header2';
 
 export default function Filters({ isOpen = false, openFilters = () => {} }) {
 	return (
-		<div className={styles.container}>
-			<div className={`${styles.box} ${isOpen && styles.openFilters}`}>
-				<CloseButton value={openFilters} />
-				{/* <span className={styles.title}>Filters</span> */}
-				<div className={styles.title}>
-					<Header2 value='Filters' />
+		<>
+			<div className={`${styles.container}`}>
+				<div className={`${styles.box} ${isOpen && styles.openFilters}`}>
+					<CloseButton value={openFilters} />
+					{/* <span className={styles.title}>Filters</span> */}
+					<div className={styles.title}>
+						<Header2 value='Filters' />
+					</div>
+					<button className={styles.monthBtn}>
+						Month
+						<span className={styles.monthBtnIcon}>
+							<FontAwesomeIcon icon='fa-solid fa-chevron-right' />
+						</span>
+					</button>
 				</div>
-				<button className={styles.monthBtn}>
-					Month
-					<span className={styles.monthBtnIcon}>
-						<FontAwesomeIcon icon='fa-solid fa-chevron-right' />
-					</span>
-				</button>
 			</div>
-		</div>
+
+			<div className={`${styles.bg} ${isOpen && styles.blur}`}></div>
+		</>
 	);
 }
