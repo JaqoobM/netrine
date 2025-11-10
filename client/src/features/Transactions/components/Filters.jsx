d18c6705915175e928046f1b98d8d3025459f78e
Jakub <jm54706@outlook.com>
2025-11-10 17:55:40 +0100

    changed filters

--- client/src/features/Transactions/components/Filters.jsx
+++ client/src/features/Transactions/components/Filters.jsx
@@ -1,13 +1,17 @@
 import styles from './Filters.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CloseButton from '../../../components/CloseButton/CloseButton';
+import Header2 from '../../../components/Header2/Header2';
 
 export default function Filters({ isOpen = false, openFilters = () => {} }) {
 	return (
 		<div className={styles.container}>
 			<div className={`${styles.box} ${isOpen && styles.openFilters}`}>
 				<CloseButton value={openFilters} />
-				<span className={styles.title}>Filters</span>
+				{/* <span className={styles.title}>Filters</span> */}
+				<div className={styles.title}>
+					<Header2 value='Filters' />
+				</div>
 				<button className={styles.monthBtn}>
 					Month
 					<span className={styles.monthBtnIcon}>
